65d2ee325ce14da07fdea93d67ea480145181d65
CortezV20 <victorandrecortez@gmail.com>
2022-10-06 12:32:17 -0500

    Prueba de personaje

--- Assets/Scrips/CameraController.cs
+++ Assets/Scrips/CameraController.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform target;//se necesita la posicion del jugador y esto me indica
+    public Vector3 offset;//sirve para sumar los vectores de la posicion del player
 
-    // Update is called once per frame
+    [Range(1,10)]
+    public float smootherFactor;
+
     void Update()
     {
-        
+        var targetPosition = target.position + offset;
+        //para que la camara se mueva unos milisegundos despues del player
+        var smootherPosition = Vector3.Lerp(transform.position, targetPosition, smootherFactor * Time.fixedDeltaTime);
+        transform.position = smootherPosition;
     }
 }
